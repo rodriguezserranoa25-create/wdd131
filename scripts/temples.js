3bdd928762519edcc2007d8baebb3131990c9dc4
rodriguezserranoa25-create <arodrguezserrano@byupathway.edu>
2025-11-08 19:09:34 +0100

    js fixes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,20 @@
-// Footer dynamic info
-document.querySelector("#year").textContent = new Date().getFullYear();
-document.querySelector("#lastModified").textContent = document.lastModified;
+(() => {
+  // Footer dynamic info
+  document.querySelector("#year").textContent = new Date().getFullYear();
+  document.querySelector("#lastModified").textContent = document.lastModified;
 
-// Hamburger menu toggle
-const menuButton = document.querySelector("#menuButton");
-const navMenu = document.querySelector("#navMenu");
+  // Hamburger menu toggle
+  const menuButton = document.querySelector("#menuButton");
+  const navMenu = document.querySelector("#navMenu");
 
-menuButton.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
+  menuButton.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
 
-  // Change symbol when toggled
-  if (navMenu.classList.contains("show")) {
-    menuButton.textContent = "✕"; // close symbol
-  } else {
-    menuButton.textContent = "☰"; // open symbol
-  }
-});
+    // Change symbol when toggled
+    if (navMenu.classList.contains("show")) {
+      menuButton.textContent = "✕"; // close symbol
+    } else {
+      menuButton.textContent = "☰"; // open symbol
+    }
+  });
+})();
